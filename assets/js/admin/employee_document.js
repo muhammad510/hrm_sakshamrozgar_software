6d2d21d587fb2e17555641eae168af39428604f0
muhammad <muhammad5101729@gmail.com>
2026-07-06 16:07:28 +0530

    some section make dynamic

--- assets/js/admin/employee_document.js
+++ assets/js/admin/employee_document.js
@@ -1,105 +1,146 @@
-$(function() 
-{
-	$(document).unbind("change").on("change", '.getSelection', function() 
-	{
-		let actNbtn = $(this).attr('data-id');
-		let curID = $(this).attr('id');
-		let getData=actNbtn.split('===');	
-		if(getData[0]=='optedSelect')
-		{
-			let target=$('#base_url').val()+getData[1];
-			$('#'+getData[2]).css('color', '#099b7e !important').html('<option>Please Wait.....</option>');
-			$.post(target,{id:$('#'+curID).val(),actnMode:getData[0]},function(htmlAmi){$('#'+getData[2]).html(htmlAmi).css('color', 'rgb(62, 62, 62) !important');});
-			}	
-			else if((getData[0]=='prevSelect')||(getData[0]=='ofrLtrSelect'))
-			{
-				let target=$('#base_url').val()+getData[1];
-				$.post(target,{id:$('#'+curID).val(),actnMode:getData[0]},function(htmlAmi)
-				{
-					if(htmlAmi.addClas=='tst_success')
-					{
-					   if(getData[0]=='prevSelect')
-					   {
-					   		$('#'+getData[2]).val(htmlAmi.department);
-					   		$('#'+getData[3]).val(htmlAmi.ctc);
-					   		$('#'+getData[4]).html(htmlAmi.isDesign);
-					   		}
-							else if(getData[0]=='ofrLtrSelect')
-					   		{
-								$('#'+getData[2]).val(htmlAmi.dtJoining);
-					   			$('#'+getData[3]).val(htmlAmi.ctc);
-								}
-					  }else{toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);}
-					},'json');
-				}
-			else if(getData[0]=='prevDesign')
-			{
-				let target=$('#base_url').val()+getData[1];
-				$.post(target,{id:$('#'+curID).val(),actn:getData[0],br_id:$('#branchFrPromo').val()},function(htmlAmi)
-				{
-					if(htmlAmi.addClas=='tst_success')
-					{
-					   $('#'+getData[2]).val(htmlAmi.ctcEmp);
-					  }else{toastMultiShow(htmlAmi.addClas,htmlAmi.msg,htmlAmi.icon);}
-					},'json');	
-				}
-			
-		
-	});
-	$(document).unbind("click").on("click", '.getClickOptn', function() 
-	{
-		let actNbtn = $(this).attr('data-id');
-		let curID = $(this).attr('id');
-		let getData=actNbtn.split('===');	
-		if(getData[0]=='getClickOptn')
-		{
-			let frmDetails='';
-			let frmBtn='';
-			if(getData[2]=='warningEmployee')
-			{	
-					let branchFrWarn=$("#branchFrWarn").val();
-					let warningEmployee=$("#warningEmployee").val();
-					let rptManager=$("#reportingManager").val();
-					let warningRegarding=$("#warningRegarding").val();
-				    frmDetails={branch: branchFrWarn,employeeID: warningEmployee,refNo: rptManager,regarding:warningRegarding};
-					frmBtn='<i class="ti-save"></i> Create Warning Letter';$('#downloadForOffrFrm').hide();$('#downloadFrm').show();
-					$("#typLetter").html('Warning Letter');
-				}
-				else if(getData[2]=='promoteEmployee')
-				{	
-					frmDetails={branch:$("#branchFrPromo").val(),employeeID:$("#preProEmployee").val(),designation:$("#promoDesignation").val(),ctc:$("#empPromoCtc").val()};
-					frmBtn='<i class="ti-save"></i> Create Promotion Letter';$('#downloadForOffrFrm').hide();$('#downloadFrm').show();
-					}
-					else if(getData[2]=='joinNewEmployee')
-					{	
-						frmDetails={branch:$("#offrBranch").val(),employeeID:$("#joinginEmployee").val(),dtJoining:$("#empDtJoining").val(),ctc:$("#offrSalary").val()};
-						frmBtn='<i class="ti-save"></i> Create Offer Letter';$('#downloadFrm').hide();$('#downloadForOffrFrm').show();$("#typLetter").html('Offer Letter');
+$(function () {
+	$(document)
+		.unbind("change")
+		.on("change", ".getSelection", function () {
+			let actNbtn = $(this).attr("data-id");
+			let curID = $(this).attr("id");
+			let getData = actNbtn.split("===");
+			if (getData[0] == "optedSelect") {
+				let target = $("#base_url").val() + getData[1];
+				$("#" + getData[2])
+					.css("color", "#099b7e !important")
+					.html("<option>Please Wait.....</option>");
+				$.post(
+					target,
+					{ id: $("#" + curID).val(), actnMode: getData[0] },
+					function (htmlAmi) {
+						$("#" + getData[2])
+							.html(htmlAmi)
+							.css("color", "rgb(62, 62, 62) !important");
+					},
+				);
+			} else if (getData[0] == "prevSelect" || getData[0] == "ofrLtrSelect") {
+				let target = $("#base_url").val() + getData[1];
+				$.post(
+					target,
+					{ id: $("#" + curID).val(), actnMode: getData[0] },
+					function (htmlAmi) {
+						if (htmlAmi.addClas == "tst_success") {
+							if (getData[0] == "prevSelect") {
+								$("#" + getData[2]).val(htmlAmi.department);
+								$("#" + getData[3]).val(htmlAmi.ctc);
+								$("#" + getData[4]).html(htmlAmi.isDesign);
+							} else if (getData[0] == "ofrLtrSelect") {
+								$("#" + getData[2]).val(htmlAmi.dtJoining);
+								$("#" + getData[3]).val(htmlAmi.ctc);
+							}
+						} else {
+							toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);
 						}
-			let target=$('#base_url').val()+getData[1];
-			$('#'+curID).html('<i class="fe fe-sun bx-spin"></i> Please Wait.....');
-			$.post(target,{actn:getData[2],details:frmDetails},function(htmlAmi)
-			{
-					
-				 $('#'+curID).html(htmlAmi.btnTxt);
-				 if(htmlAmi.addClas=='tst_success')
-				 {
-					if(getData[2]=='joinNewEmployee')
-					{$("#downloadForOffrFrm").attr("data-id",htmlAmi.targetLink);}
-					
-					
-					 if(getData[2]=='promoteEmployee'){toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);}
-					 else{$('#document_printArea').modal('show');$('#printDataArea').html(htmlAmi.printData);}
-
-					 }else{toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);}
-					 $('#'+curID).html(frmBtn);
-				 },'json');
+					},
+					"json",
+				);
+			} else if (getData[0] == "prevDesign") {
+				let target = $("#base_url").val() + getData[1];
+				$.post(
+					target,
+					{
+						id: $("#" + curID).val(),
+						actn: getData[0],
+						br_id: $("#branchFrPromo").val(),
+					},
+					function (htmlAmi) {
+						if (htmlAmi.addClas == "tst_success") {
+							$("#" + getData[2]).val(htmlAmi.ctcEmp);
+						} else {
+							toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);
+						}
+					},
+					"json",
+				);
 			}
-		else if(getData[0]=='download')
-		{
-			 $('#'+curID).html('<i class="fe fe-sun bx-spin"></i> Please Wait.....');
-			 setTimeout(function(){$('#'+curID).html('<i class="si si-printer"></i> Print ');window.open(getData[1],'_blank');},3000);
-			}		
-		
-	});
-	
+		});
+	$(document)
+		.unbind("click")
+		.on("click", ".getClickOptn", function () {
+			let actNbtn = $(this).attr("data-id");
+			let curID = $(this).attr("id");
+			let getData = actNbtn.split("===");
+			if (getData[0] == "getClickOptn") {
+				let frmDetails = "";
+				let frmBtn = "";
+				if (getData[2] == "warningEmployee") {
+					let branchFrWarn = $("#branchFrWarn").val();
+					let warningEmployee = $("#warningEmployee").val();
+					let rptManager = $("#reportingManager").val();
+					let warningRegarding = $("#warningRegarding").val();
+					frmDetails = {
+						branch: branchFrWarn,
+						employeeID: warningEmployee,
+						refNo: rptManager,
+						regarding: warningRegarding,
+					};
+					frmBtn = '<i class="ti-save"></i> Create Warning Letter';
+					$("#downloadForOffrFrm").hide();
+					$("#downloadFrm").show();
+					$("#typLetter").html("Warning Letter");
+				} else if (getData[2] == "promoteEmployee") {
+					frmDetails = {
+						branch: $("#branchFrPromo").val(),
+						employeeID: $("#preProEmployee").val(),
+						designation: $("#promoDesignation").val(),
+						ctc: $("#empPromoCtc").val(),
+					};
+					frmBtn = '<i class="ti-save"></i> Create Promotion Letter';
+					$("#downloadForOffrFrm").hide();
+					$("#downloadFrm").show();
+				} else if (getData[2] == "joinNewEmployee") {
+					frmDetails = {
+						branch: $("#offrBranch").val(),
+						employeeID: $("#joinginEmployee").val(),
+						dtJoining: $("#empDtJoining").val(),
+						ctc: $("#offrSalary").val(),
+					};
+					frmBtn = '<i class="ti-save"></i> Create Offer Letter';
+					$("#downloadFrm").hide();
+					$("#downloadForOffrFrm").show();
+					$("#typLetter").html("Offer Letter");
+				}
+				let target = $("#base_url").val() + getData[1];
+				$("#" + curID).html(
+					'<i class="fe fe-sun bx-spin"></i> Please Wait.....',
+				);				
+				$.post(
+					target,
+					{ actn: getData[2], details: frmDetails },
+					function (htmlAmi) {
+						$("#" + curID).html(htmlAmi.btnTxt);
+						if (htmlAmi.addClas == "tst_success") {
+							if (getData[2] == "joinNewEmployee") {
+								$("#downloadForOffrFrm").attr("data-id", htmlAmi.targetLink);
+							}
+
+							if (getData[2] == "promoteEmployee") {
+								toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);
+							} else {
+								$("#document_printArea").modal("show");
+								$("#printDataArea").html(htmlAmi.printData);
+							}
+						} else {
+							toastMultiShow(htmlAmi.addClas, htmlAmi.msg, htmlAmi.icon);
+						}
+						$("#" + curID).html(frmBtn);
+					},
+					"json",
+				);
+			} else if (getData[0] == "download") {
+				$("#" + curID).html(
+					'<i class="fe fe-sun bx-spin"></i> Please Wait.....',
+				);
+				setTimeout(function () {
+					$("#" + curID).html('<i class="si si-printer"></i> Print ');
+					window.open(getData[1], "_blank");
+				}, 3000);
+			}
+		});
 });
--- system/helpers/document_helper.php
+++ system/helpers/document_helper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -36,7 +37,7 @@
  * @since	Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * CodeIgniter Email Helpers
@@ -51,20 +52,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // ------------------------------------------------------------------------
 function warning_letter($where)
 {
-$printPage='<div class="col-md-12">
+	$printPage = '<div class="col-md-12">
 			  <div class="row row-sm">
-				<div class="slipImg"> <img src="'.base_url('uploads/company/payslip_heading.png').'"> </div>
+				<div class="slipImg">
+				<div class="row">
+				<div class="col-md-4">
+				<img src="' . base_url(system_info('favicon')) . '" style="height:100px;width:auto;text-align:right">
+				</div>
+				<div class="col-md-8">
+				<h3 style="text-align:center">'.system_info('company_name').'</h3>
+				<h6 style="text-align:center">'.system_info('company_address').'</h6>
+				</div>
+
+				</div>				 
+				 </div>
 				<div class="parent">
-					<div class="child child-1"><strong> Ref. No. - </strong>'.$where['refNo'].'</div>
+					<div class="child child-1"><strong> Ref. No. - </strong>' . $where['refNo'] . '</div>
 					<div class="child child-2">Warning Letter</div>
-					<div class="child child-3"><p><strong>Date : </strong>'.date('d/m/Y').'</p></div>
+					<div class="child child-3"><p><strong>Date : </strong>' . date('d/m/Y') . '</p></div>
 				</div>
 				
 				
 				<div class="letter-content wrningLtr">
-				  <p>Dear <strong>'.$where['empName'].'</strong>,</p>
-				  <p>This letter is to formally notify you that your recent behavior/performance has been found to be unsatisfactory and against the policies of the company. Specifically, <strong>'.$where['reason'].'</strong>.</p>
-				  <p>On <strong>'.date('d F Y').'</strong>, you have <strong>'.$where['reason'].'</strong>. This is a serious concern because it [mention the consequences of their actions on the team or <strong>'.system_info('company_name').'</strong>.</p>
+				  <p>Dear <strong>' . $where['empName'] . '</strong>,</p>
+				  <p>This letter is to formally notify you that your recent behavior/performance has been found to be unsatisfactory and against the policies of the company. Specifically, <strong>' . $where['reason'] . '</strong>.</p>
+				  <p>On <strong>' . date('d F Y') . '</strong>, you have <strong>' . $where['reason'] . '</strong>. This is a serious concern because it [mention the consequences of their actions on the team or <strong>' . system_info('company_name') . '</strong>.</p>
 				  <p>We have already discussed this matter with you on [mention previous discussions, if any] and have provided you with an opportunity to improve. However, the situation has not improved, and it is now affecting the overall work environment.</p>
 				  <p>This letter serves as an official warning. We expect you to [mention the expected behavior or improvement], and any further violation may lead to [mention possible consequences, like further disciplinary action or termination].</p>
 				  <p>We sincerely hope that you will take this warning seriously and make necessary improvements to avoid any further consequences.</p>
@@ -72,41 +84,38 @@ $printPage='<div class="col-md-12">
 				  <p>Thank you for your attention to this matter.</p>
 				  <div class="signature">
 					<p>Regards,</p>
-					<p><strong>'.$where['managerNm'].'</strong></p>
+					<p><strong>' . $where['managerNm'] . '</strong></p>
 					<p><em>Reporting Manager</em></p>
-					<p>'.system_info('company_name').' | '.config_item('address').' | '.config_item('mobile_number').'</p>
+					<p>' . system_info('company_name') . ' | ' . config_item('address') . ' | ' . config_item('mobile_number') . '</p>
 				  </div>
 				</div>
 			  </div>
 			</div>';
-		
+
 	return $printPage;
-		
-	
-	
-} 
+}
 
 function offer_letter($where)
 {
-$printPage='<div class="offrHdrImg">
-				<img src="'.base_url('uploads/company/offer_letter.png').'"> </div>
+	$printPage = '<div class="offrHdrImg">
+				<img src="' . base_url('uploads/company/offer_letter.png') . '"> </div>
 			  <div class="parent mrvPd">
-				<div class="child child-1"><strong> Ref. No. - </strong>'.$where['jobReqNo'].'</div>
+				<div class="child child-1"><strong> Ref. No. - </strong>' . $where['jobReqNo'] . '</div>
 				<div class="child child-2">Offer Letter</div>
 				<div class="child child-3">
-				  <p><strong>Date : </strong>'.date('d/m/Y').'</p>
+				  <p><strong>Date : </strong>' . date('d/m/Y') . '</p>
 				</div>
 			  </div>
 			  <div id="getPrfrmList">
 				<div class="appointment-letter">
-				  <p>Mr./Ms. <span class="empFlName myFontBg">'.$where['empName'].'</span> <br/>
-					<span class="myFontBg" id="empAddr">'.$where['empAddr'].'</span><br/>
-					City-<span class="myFontBg" id="empCity">'.$where['distState'].'</span> ,<br />
-					PIN <span class="myFontBg" id="empZipCode">'.$where['zipcode'].'</span></p>
-				  <p> Dear <span class="empFlName myFontBg">'.$where['empName'].'</span>, </p>
-				  <p> It is our pleasure to extend the following offer of employment to you on behalf of <span class="myFontBg">'.system_info('company_name').'</span>. </p>
-				  <p>further to the interview and discussions you have had with us. You are appointed to the position of "<span class="myFontBg empDesign">'.$where['designation'].'</span>" at  "<span class="myFontBg empDepartment">'.$where['branchNm'].'</span>" and you are expected to join duty on <span class="myFontBg">'.$where['empJoining'].'</span> under the following terms and conditions: </p>
-				  <p>You will be on probation period (<strong>'.$where['empJoining'].'</strong>) for 6 months and on satisfactory completion of the probation period you will be entitled for the confirmation. </p>
+				  <p>Mr./Ms. <span class="empFlName myFontBg">' . $where['empName'] . '</span> <br/>
+					<span class="myFontBg" id="empAddr">' . $where['empAddr'] . '</span><br/>
+					City-<span class="myFontBg" id="empCity">' . $where['distState'] . '</span> ,<br />
+					PIN <span class="myFontBg" id="empZipCode">' . $where['zipcode'] . '</span></p>
+				  <p> Dear <span class="empFlName myFontBg">' . $where['empName'] . '</span>, </p>
+				  <p> It is our pleasure to extend the following offer of employment to you on behalf of <span class="myFontBg">' . system_info('company_name') . '</span>. </p>
+				  <p>further to the interview and discussions you have had with us. You are appointed to the position of "<span class="myFontBg empDesign">' . $where['designation'] . '</span>" at  "<span class="myFontBg empDepartment">' . $where['branchNm'] . '</span>" and you are expected to join duty on <span class="myFontBg">' . $where['empJoining'] . '</span> under the following terms and conditions: </p>
+				  <p>You will be on probation period (<strong>' . $where['empJoining'] . '</strong>) for 6 months and on satisfactory completion of the probation period you will be entitled for the confirmation. </p>
 				  <p>This offer will be subject to the standard terms and conditions of employment by <strong>ASHUTOSH</strong> and also will be governed by the policies, rules and guidelines of the Company.</p>
 				  <p  style="text-align:justify">Please note that this offer is valid subject to your signing (authentic signature Pl.) and returning the duplicate copy of this letter within 3 working days with your Date of Joining. You will be given a remuneration as per CTC structure attached.You will not be entitled for any kind of leave except one casual leave during Probation period.</p>
 				  <p  style="text-align:justify">That your appointment will be further subject to the verification of your credentials, testimonials and other particulars mentioned by you in your application at the time of your appointment. In case it comes to the notice of the Management that the particulars given by you in your appointment were wrong or concealed, your appointment shall be rendered void ab initio and will, therefore, be deemed cancelled automatically.You will be issued a detailed appointment letter after your confirmation at the concerned branch offices and upon completion of joining formalities.Further, during the course of your employment, if it is found that you have been indulging into any kind of irregularity, process lapse, fraud and misappropriation, the Company reserves the right to take appropriate action including immediate termination of your employment.
@@ -121,43 +130,43 @@ $printPage='<div class="offrHdrImg">
 				  <div class="page-break"></div>
 				  <div style="float:right;width:35%;"><span style="font-size:11px;">I have read the above terms and conditions of my appointment. I accept the same.</span><br/><br/><br/><br/>
 					_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _<br/><br/><br/>
-					Applicant Name <strong>'.$where['empName'].'</strong><br/><br/>
-					Employee Code <strong>'.$where['empCode'].'</strong></div>
-				 <div class="offrFtrImg"> <img src="'.base_url('uploads/company/offer_bottom.png').'"> </div>
+					Applicant Name <strong>' . $where['empName'] . '</strong><br/><br/>
+					Employee Code <strong>' . $where['empCode'] . '</strong></div>
+				 <div class="offrFtrImg"> <img src="' . base_url('uploads/company/offer_bottom.png') . '"> </div>
 				  <p style="clear: both;">Encl: <span class="myFontBg"> 1) Service Agreement </span></p>
 				  <table id="payFormat">
 					<tr><td colspan="2"  style="text-align:center"><span class="myFontBg">ANNEXURE I</span></td></tr>
-					<tr><td style="text-align:center" colspan="2"><span class="myFontBg">'.system_info('company_name').'</span></td></tr>
-					<tr><td style="width:40%">Employee No </td><td><span class="myFontBg">'.$where['empCode'].'</span></td></tr>
-					<tr><td style="width:40%">Name </td><td><span class="myFontBg" style="text-transform:uppercase;">'.$where['empName'].'</span></td></tr>
-					<tr><td style="width:40%">Designation </td><td><span class="myFontBg" style="text-transform:uppercase;">'.$where['designation'].'</span></td></tr>
-					<tr><td style="width:40%">Department </td><td><span class="myFontBg" style="text-transform:uppercase;">'.$where['department'].'</span></td></tr>
-					<tr><td style="width:40%">Date of Joining </td><td><span class="myFontBg">'.$where['empJoining'].'</span></td></tr>
+					<tr><td style="text-align:center" colspan="2"><span class="myFontBg">' . system_info('company_name') . '</span></td></tr>
+					<tr><td style="width:40%">Employee No </td><td><span class="myFontBg">' . $where['empCode'] . '</span></td></tr>
+					<tr><td style="width:40%">Name </td><td><span class="myFontBg" style="text-transform:uppercase;">' . $where['empName'] . '</span></td></tr>
+					<tr><td style="width:40%">Designation </td><td><span class="myFontBg" style="text-transform:uppercase;">' . $where['designation'] . '</span></td></tr>
+					<tr><td style="width:40%">Department </td><td><span class="myFontBg" style="text-transform:uppercase;">' . $where['department'] . '</span></td></tr>
+					<tr><td style="width:40%">Date of Joining </td><td><span class="myFontBg">' . $where['empJoining'] . '</span></td></tr>
 					<tr><td style="width:40%">Remarks </td><td></td></tr>
 					<tr class="baComponent"><td style="width:40%">SALARY COMPONENTS </td><td>INR PER MONTH</td></tr>
-					<tr><td style="width:40%">Basic </td><td><span class="myFontBg">'.$where['empBasic'].'</span></td></tr>
-					<tr><td style="width:40%">HRA </td><td><span class="myFontBg">'.$where['empHRA'].'</span></td></tr>
-					<tr><td style="width:40%">Transportation Allowance</td><td><span class="myFontBg">'.$where['empTA'].'</span></td></tr>
-					<tr><td style="width:40%">Professional Development Allowance </td><td><span class="myFontBg">'.$where['empPA'].'</span></td></tr>
-					<tr><td style="width:40%">Education Allowance </td><td><span class="myFontBg">'.$where['empEA'].'</span></td></tr>
-					<tr><td style="width:40%">INTERIM Bonus </td><td><span class="myFontBg">'.$where['empBonus'].'</span></td></tr>
-					<tr><td style="width:40%">Specical Allowance </td><td><span class="myFontBg">'.$where['empSpcialA'].'</span></td></tr>
+					<tr><td style="width:40%">Basic </td><td><span class="myFontBg">' . $where['empBasic'] . '</span></td></tr>
+					<tr><td style="width:40%">HRA </td><td><span class="myFontBg">' . $where['empHRA'] . '</span></td></tr>
+					<tr><td style="width:40%">Transportation Allowance</td><td><span class="myFontBg">' . $where['empTA'] . '</span></td></tr>
+					<tr><td style="width:40%">Professional Development Allowance </td><td><span class="myFontBg">' . $where['empPA'] . '</span></td></tr>
+					<tr><td style="width:40%">Education Allowance </td><td><span class="myFontBg">' . $where['empEA'] . '</span></td></tr>
+					<tr><td style="width:40%">INTERIM Bonus </td><td><span class="myFontBg">' . $where['empBonus'] . '</span></td></tr>
+					<tr><td style="width:40%">Specical Allowance </td><td><span class="myFontBg">' . $where['empSpcialA'] . '</span></td></tr>
 					<tr>
-					  <td style="width:40%">GROSS SALARY</td><td><span class="myFontBg">'.$where['empGross'].'</span></td></tr>
-					<tr><td style="width:40%">PF-Contribution Employee</td><td><span class="myFontBg">'.$where['empPF'].'</span></td></tr>
-					<tr><td style="width:40%">PF-Contribution Employer</td><td><span class="myFontBg">'.$where['empPF'].'</span></td></tr>
-					<tr><td style="width:40%">PT </td><td><span class="myFontBg">'.$where['empPTA'].'</span></td></tr>
-					<tr><td style="width:40%">ESIC Employee </td><td><span class="myFontBg">'.$where['empESIC'].'</span></td></tr>
-					<tr><td style="width:40%">ESIC Employer </td><td><span class="myFontBg">'.$where['empLoyerESIC'].'</span></td></tr>
+					  <td style="width:40%">GROSS SALARY</td><td><span class="myFontBg">' . $where['empGross'] . '</span></td></tr>
+					<tr><td style="width:40%">PF-Contribution Employee</td><td><span class="myFontBg">' . $where['empPF'] . '</span></td></tr>
+					<tr><td style="width:40%">PF-Contribution Employer</td><td><span class="myFontBg">' . $where['empPF'] . '</span></td></tr>
+					<tr><td style="width:40%">PT </td><td><span class="myFontBg">' . $where['empPTA'] . '</span></td></tr>
+					<tr><td style="width:40%">ESIC Employee </td><td><span class="myFontBg">' . $where['empESIC'] . '</span></td></tr>
+					<tr><td style="width:40%">ESIC Employer </td><td><span class="myFontBg">' . $where['empLoyerESIC'] . '</span></td></tr>
 					<tr><td style="width:40%">Income Tax</td><td><span class="myFontBg">0</span></td></tr>
-					<tr><td style="width:40%">Net Pay</td><td><span class="myFontBg">'.$where['netPay'].'</span></td></tr>
-					<tr><td style="width:40%">CTC </td><td><span class="myFontBg">'.$where['empCTC'].'</span></td></tr>
-					<tr><td style="width:40%">&nbsp;</td><td><span class="myFontBg" style="text-transform:capitalize">'.$where['empCTCWord'].'</span></td></tr>
+					<tr><td style="width:40%">Net Pay</td><td><span class="myFontBg">' . $where['netPay'] . '</span></td></tr>
+					<tr><td style="width:40%">CTC </td><td><span class="myFontBg">' . $where['empCTC'] . '</span></td></tr>
+					<tr><td style="width:40%">&nbsp;</td><td><span class="myFontBg" style="text-transform:capitalize">' . $where['empCTCWord'] . '</span></td></tr>
 				   </table>
-				  <p style="padding-top: 15px;">We at <strong>'.system_info('company_name').'</strong> would like to create an environment and culture committed to co-operation,quality and responsiveness that permits every activity.</p>
-				  <p>We take this oppertunity to wish you the very best in your tenure with tenure of contact with <strong>'.system_info('company_name').'</strong>.</p>
+				  <p style="padding-top: 15px;">We at <strong>' . system_info('company_name') . '</strong> would like to create an environment and culture committed to co-operation,quality and responsiveness that permits every activity.</p>
+				  <p>We take this oppertunity to wish you the very best in your tenure with tenure of contact with <strong>' . system_info('company_name') . '</strong>.</p>
 				  <p>Your sincerely,</p>
-				  <p>  For <span class="myFontBg" style="text-transform:uppercase;">'.system_info('company_name').'</span> </p>
+				  <p>  For <span class="myFontBg" style="text-transform:uppercase;">' . system_info('company_name') . '</span> </p>
 				  <p>Name,<br/><br/><br/>
 					Authorised Signatory</p>
 				   <p>Please indicates your acceptance of the terms by signing and returning the duplicates copy hereof</p>
@@ -165,17 +174,14 @@ $printPage='<div class="offrHdrImg">
 				   <p>SIGNATURE WITH DATE :-<span style="font-weight:700;">________________________________________</span></p>
 				</div>
 			  </div>';
-		
+
 	return $printPage;
-		
-	
-	
-} 
+}
 
 
 function testMode()
 {
-$printPage=' <div class="header">
+	$printPage = ' <div class="header">
         <h1>Warning Letter</h1>
         <p>Date: <span id="date"></span></p>
     </div>
@@ -218,9 +224,6 @@ $printPage=' <div class="header">
     document.getElementById("occurrence-date").textContent = "January 15, 2025"; // Date of Occurrence
     document.getElementById("manager-name").textContent = "Jane Smith"; // Managers Name
 </script>';
-		
+
 	return $printPage;
-		
-	
-	
-} 
+}
